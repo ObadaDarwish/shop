7b79a8e060b5621e8cc8d25922b83723b3d5a237
ObadaDarwish <obada_567@hotmail.co.uk>
2020-06-15 20:57:06 +0200

    minor change

--- app.js
+++ app.js
@@ -38,13 +38,14 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
     next();
 });
-app.use('/', (req, res, next) => {
-    res.send('Server is up and running!');
-});
+
 app.use('/admin', isAuth, adminRoute);
 app.use('/user', users);
 app.use(auth);
 app.use(isAuth, shop);
+app.use('/', (req, res, next) => {
+    res.send('Server is up and running!');
+});
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
